6d402649dec62364a3845e09180e33014db45fc8
gyhmac <gyeonho@gmail.com>
2025-04-29 09:42:50 +0900

    :memo: jwt token

--- backend_api/routes/auth.js
+++ backend_api/routes/auth.js
@@ -4,6 +4,7 @@ const User = require("../models/user");
 // express 모듈을 가져옵니다.
 const bcrypt = require('bcryptjs');
 const authRouter = express.Router();
+const jwt = require('jsonwebtoken');
 // 회원가입 라우터를 정의합니다.POST 요청을 처리하는 "/api/signup" 경로를 정의합니다.클라이언트가 회원가입 정보를 전송하면 이 핸들러가 실행됩니다.
 authRouter.post("/api/signup", async (req, res) => {
   // 클라이언트가 전송한 회원가입 정보를 req.body에서 가져옵니다.fullname, email, password를 추출합니다.
@@ -46,3 +47,36 @@ module.exports = authRouter;
 //
 // 2. `var` 대신 `let` 또는 `const`를 사용하는 것이 일반적으로 더 안전함
 // --------------------------------------------
+
+authRouter.post("/api/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // 이메일과 비밀번호를 사용하여 사용자를 찾습니다.
+    const user = await User.findOne({ email });
+    // 사용자가 존재하지 않으면 400 상태 코드와 함께 에러 메시지를 반환합니다.
+    if (!user) {
+      return res.status(400).json({ msg: "가입한 사용자가 없습니다." });
+    }
+    // 비밀번호가 일치하는지 확인합니다.
+    const isMatch = await bcrypt.compare(password, user.password);
+    // 비밀번호가 일치하지 않으면 400 상태 코드와 함께 에러 메시지를 반환합니다.
+    if (!isMatch) {
+      return res.status(400).json({ msg: "패스워드가 일치하지 않습니다." });
+    } else{
+    
+      const token = jwt.sign({ id: user._id}, "passwordKey")
+      
+      const {password, ...userWithoutPassword} = user._doc;
+      
+      res.json({token, ...userWithoutPassword});
+    // 비밀번호가 일치하면 JWT 토큰을 생성합니다.
+    }
+    // 비밀번호가 일치하면 사용자 정보를 반환합니다.
+    
+  } catch (error) { 
+    // 에러가 발생하면 500 상태 코드와 함께 에러 메시지를 반환합니다.
+    return res.status(500).json({ error: error.message });
+  }
+}
+);
+// 이 라우터를 사용하여 로그인 기능을 구현할 수 있습니다.
