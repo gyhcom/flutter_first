0b0c776502c9756f660412ac35956d3749111d5d
gyhmac <gyeonho@gmail.com>
2025-05-12 13:17:55 +0900

    feat: update HomeScreen layout to include HeaderWidget and enable scrolling

--- lib/views/screens/nav_screens/home_screen.dart
+++ lib/views/screens/nav_screens/home_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:store_app/views/screens/nav_screens/widgets/header_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Home Screen')));
+    return const Scaffold(
+      body: SingleChildScrollView(child: Column(children: [HeaderWidget()])),
+    );
   }
 }
