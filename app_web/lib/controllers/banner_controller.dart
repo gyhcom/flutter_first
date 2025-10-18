7d6cc1659c813a8d84bb572bf1e829d5f21a9678
gyhmac <gyeonho@gmail.com>
2025-10-18 23:58:34 +0900

    FEAT: banner_Controller

--- app_web/lib/controllers/banner_controller.dart
+++ app_web/lib/controllers/banner_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:app_web/models/banner.dart';
 import 'package:app_web/services/manage_http_response.dart';
 import 'package:cloudinary_public/cloudinary_public.dart';
@@ -42,6 +44,29 @@ class BannerController {
       );
     } catch (e) {
       print(e);
+    }
+
+    //fetch banners
+
+    Future<List<BannerModel>> loadBanners() async {
+      try {
+        //send an http get request to fetch banners
+        http.Response response = await http.get(
+          Uri.parse("$uri/api/banner"),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+        );
+
+        print(response.body);
+        if (response.statusCode == 200) {
+          //ok
+          List<dynamic> data = jsonDecode(response.body);
+          List<BannerModel> banners =
+              data.map((banner) => BannerModel.fromJson(banner)).toList();
+          return banners;
+        }
+      } catch (e) {}
     }
   }
 }
